7327678b34efc1f164dd0bebf26f4ea039a68232
Kyrylo Yezholov <kirill@ezh.com.ua>
2025-05-17 14:27:48 +0300

    Commit 3 (README)

--- Agent/obj/Debug/net9.0/Agent.AssemblyInfo.cs
+++ Agent/obj/Debug/net9.0/Agent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6f1fc3162a391775f5d3bfb1d4b4ba0ca93836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df551d2bf7909dce9202dd6264933320fc531bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 **Commit 1**: Add File Structure to Agent and Master
 
 **Commit 2**: A basic asynchronous Agent that indexes files has been created. Sample test text has been automatically generated for testing the application. To test this version, type: `dotnet run ../TestData`
+
+**Commit 3:** Created a basic PipeClient in Agent for communication between programs. Update `Program.cs.` To test this version, type: `dotnet run ../TestData <AgentID>.`
